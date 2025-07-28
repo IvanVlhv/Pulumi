1a902bb6d4ae35bf89112be732798cb31001a8b5
Ivan Vlahović <ivan.vlahovic@reversinglabs.com>
2025-07-28 18:12:40 +0200

    main fix

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
+import os
 import pulumi
+
 from alb import Alb
 from db import Db
 from nat import Nat
@@ -7,9 +9,7 @@ from vpc import Vpc
 from web import Web
 import pulumi_std as std
 
-
-def not_implemented(msg):
-    raise NotImplementedError(msg)
+module_path = os.path.dirname(os.path.abspath(__file__))
 
 config = pulumi.Config()
 project_name = config.require_object("projectName")
@@ -25,47 +25,71 @@ db_username = config.require_object("dbUsername")
 db_password = config.require_object("dbPassword")
 web_instance_type = config.require_object("webInstanceType")
 key_name = config.require_object("keyName")
-vpc = Vpc("vpc", {
-    'awsRegion': aws_region, 
-    'projectName': project_name, 
-    'vpcCidr': vpc_cidr, 
-    'pubSubNat1': pub_sub_nat1, 
-    'pubSubNat2': pub_sub_nat2, 
-    'privSubWeb1': priv_sub_web1, 
-    'privSubWeb2': priv_sub_web2, 
-    'privSubDb1': priv_sub_db1, 
-    'privSubDb2': priv_sub_db2})
-nat = Nat("nat", {
-    'igwId': vpc.igw_id, 
-    'vpcId': vpc.vpc_id, 
-    'pubSubNat1': vpc.pub_sub_nat1_id, 
-    'pubSubNat2': vpc.pub_sub_nat2_id, 
-    'privSubWeb1': vpc.priv_sub_web1_id, 
-    'privSubWeb2': vpc.priv_sub_web2_id, 
-    'privSubDb1': vpc.priv_sub_db1_id, 
-    'privSubDb2': vpc.priv_sub_db2_id})
-sec_group = SecGroup("secGroup", {
-    'vpcId': vpc.vpc_id})
-alb = Alb("alb", {
-    'projectName': vpc.project_name, 
-    'vpcId': vpc.vpc_id, 
-    'albSecGroupId': sec_group.alb_sec_group_id, 
-    'pubSubNat1': vpc.pub_sub_nat1_id, 
-    'pubSubNat2': vpc.pub_sub_nat2_id})
-web = Web("web", {
-    'projectName': vpc.project_name, 
-    'webSecGroupId': sec_group.web_sec_group_id, 
-    'privSubWeb1': vpc.priv_sub_web1_id, 
-    'privSubWeb2': vpc.priv_sub_web2_id, 
-    'targetGroupArn': alb.target_group_arn, 
-    'instanceType': web_instance_type, 
-    'keyName': key_name, 
-    'userData': std.index.filebase64(input=f"{not_implemented('path.module')}/install_snakegame.sh")["result"]})
-db = Db("db", {
-    'projectName': vpc.project_name, 
-    'dbUsername': db_username, 
-    'dbPassword': db_password, 
-    'dbSecGroupId': sec_group.db_sec_group_id, 
-    'privSubDb1': vpc.priv_sub_db1_id, 
-    'privSubDb2': vpc.priv_sub_db2_id})
-pulumi.export("albDns", alb.alb_dns)
+
+vpc = Vpc(
+    "vpc",
+    {
+        "awsRegion": aws_region,
+        "projectName": project_name,
+        "vpcCidr": vpc_cidr,
+        "pubSubNat1": pub_sub_nat1,
+        "pubSubNat2": pub_sub_nat2,
+        "privSubWeb1": priv_sub_web1,
+        "privSubWeb2": priv_sub_web2,
+        "privSubDb1": priv_sub_db1,
+        "privSubDb2": priv_sub_db2,
+    },
+)
+nat = Nat(
+    "nat",
+    {
+        "igwId": vpc.igwId,
+        "vpcId": vpc.vpcId,
+        "pubSubNat1": vpc.pubSubNat1Id,
+        "pubSubNat2": vpc.pubSubNat2Id,
+        "privSubWeb1": vpc.privSubWeb1Id,
+        "privSubWeb2": vpc.privSubWeb2Id,
+        "privSubDb1": vpc.privSubDb1Id,
+        "privSubDb2": vpc.privSubDb2Id,
+    },
+)
+sec_group = SecGroup("secGroup", {"vpcId": vpc.vpcId})
+alb = Alb(
+    "alb",
+    {
+        "projectName": vpc.projectName,
+        "vpcId": vpc.vpcId,
+        "albSecGroupId": sec_group.albSecGroupId,
+        "pubSubNat1": vpc.pubSubNat1Id,
+        "pubSubNat2": vpc.pubSubNat2Id,
+    },
+)
+
+user_data_file = os.path.join(module_path, "install_snakegame.sh")
+
+web = Web(
+    "web",
+    {
+        "projectName": vpc.projectName,
+        "webSecGroupId": sec_group.webSecGroupId,
+        "privSubWeb1": vpc.privSubWeb1Id,
+        "privSubWeb2": vpc.privSubWeb2Id,
+        "targetGroupArn": alb.targetGroupArn,
+        "instanceType": web_instance_type,
+        "keyName": key_name,
+        "userData": std.index.filebase64(input=user_data_file)["result"],
+    },
+)
+db = Db(
+    "db",
+    {
+        "projectName": vpc.projectName,
+        "dbUsername": db_username,
+        "dbPassword": db_password,
+        "dbSecGroupId": sec_group.dbSecGroupId,
+        "privSubDb1": vpc.privSubDb1Id,
+        "privSubDb2": vpc.privSubDb2Id,
+    },
+)
+pulumi.export("albDns", alb.albDns)
+
